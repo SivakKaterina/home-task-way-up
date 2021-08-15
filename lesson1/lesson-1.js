bd9e2849144446bb2a9ba9e46729d6edfcb8197b
SivakKaterina <72225534+SivakKaterina@users.noreply.github.com>
2021-08-15 20:15:25 +0300

    Исправила ошибку в 5 задании 1 урока

--- lesson1/lesson-1.js
+++ lesson1/lesson-1.js
@@ -4,20 +4,20 @@ const town = "Мелитополь",
   population = 153,
   footballStadium = true;
 const resalt = `Я живу в городе ${town}, ${region} область. Численоость населения ${population} тысячи и есть футбольный стадион!`
-console.log(resalt);
+// console.log(resalt);
 
 // Задание 2:
 const height = 40;
 const width = 70;
 const RectangleArea = height * width;
-console.log(`Площадь прямоугольника составляет ${RectangleArea} см`);
+// console.log(`Площадь прямоугольника составляет ${RectangleArea} см`);
 
 //Задание 3:
 const time = 2;
 const speedOfFirst = 95;
 const speedOfSecond = 114;
 const DistanceСities = (speedOfFirst * time) + (speedOfSecond * time);
-console.log(`Расстояние между городами ${DistanceСities} км`);
+// console.log(`Расстояние между городами ${DistanceСities} км`);
 
 // Задание 4:
 
@@ -34,10 +34,10 @@ console.log("randomNumber больше 20, и меньше 50");
 
 //  Задание 5:
 
-switch (randomNumber) {
-  case '< 20': console.log("randomNumber меньше 20");
+switch (true) {
+  case randomNumber < 20: console.log("randomNumber меньше 20");
     break;
-  case '> 50': console.log("randomNumber больше 50");
+  case randomNumber > 50: console.log("randomNumber больше 50");
     break;
   default : console.log("randomNumber больше 20, и меньше 50");
 }
